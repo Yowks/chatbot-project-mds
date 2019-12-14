9f91be5170940a867be3b0b27f1a11160c723135
Yowks <48799889+Yowks@users.noreply.github.com>
2019-12-14 14:11:30 +0100

    :Start Sprint1  Structure complete

--- src/index.js
+++ src/index.js
@@ -7,15 +7,34 @@ class Users {
     this.i = i;
     const left = document.querySelector('#left');
     const divUser = document.createElement('div');
-    divUser.setAttribute('class', 'eachUser');
+    const divUserPhoto = document.createElement('div');
+    const imgUserPhoto = document.createElement('img');
+    const divUserInfo = document.createElement('div');
+    const divUserInfoName = document.createElement('h2');
     const name = 'name';
+    const img = 'name_photo_user';
+    const src = 'src/img/';
     const nameCreate = document.createTextNode(this.data.users[this.i][name]);
-    divUser.appendChild(nameCreate);
+    const imgValue = this.data.users[this.i][img];
+    divUser.setAttribute('class', 'eachUser');
+    divUserPhoto.setAttribute('class', 'eachUserPhoto');
+    imgUserPhoto.setAttribute('src', `${src}${imgValue}`);
+    divUserInfo.setAttribute('class', 'eachUserInfo');
+    divUser.appendChild(divUserPhoto);
+    divUser.appendChild(divUserInfo);
+    divUserPhoto.appendChild(imgUserPhoto);
+    divUserInfo.appendChild(divUserInfoName);
+    divUserInfoName.appendChild(nameCreate);
     left.appendChild(divUser);
   }
 }
-const mydata = JSON.parse(JSON.stringify(myJsonData));
-const users = new Users();
-for (let i = 0; i < mydata.users.length; i += 1) {
-  users.usersDisplay(mydata, i);
+
+function init() {
+  const mydata = JSON.parse(JSON.stringify(myJsonData));
+  const users = new Users();
+  for (let i = 0; i < mydata.users.length; i += 1) {
+    users.usersDisplay(mydata, i);
+  }
 }
+
+init();

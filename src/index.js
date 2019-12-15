e9542b373237fb32a9688f20316a4b26af31e23a
Yowks <48799889+Yowks@users.noreply.github.com>
2019-12-16 00:37:46 +0100

    :Add message view and begin message send

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './index.scss';
 import myJsonData from './data.json';
+import myJsonMessages from './dataMessages.json';
 
 class Users {
+  /* Display user information and creation of div to append the data */
   usersDisplay(data, i) {
     this.data = data;
     this.i = i;
@@ -31,6 +33,7 @@ class Users {
     left.appendChild(divUser);
   }
 
+  /* Display personnal information and creation of div to append the data */
   personnal(data) {
     this.data = data;
     const personnal = document.querySelector('#personnal');
@@ -41,9 +44,12 @@ class Users {
     const name = 'name';
     const img = 'name_photo_user';
     const src = 'src/img/';
+    const id = 'id_user';
     const nameCreate = document.createTextNode(this.data.me[0][name]);
     const imgValue = this.data.me[0][img];
+    const idValue = this.data.me[0][id];
     personnal.setAttribute('class', 'user');
+    personnal.setAttribute('data', `${idValue}`);
     divUserPhoto.setAttribute('class', 'userPhoto');
     imgUserPhoto.setAttribute('src', `${src}${imgValue}`);
     divUserInfo.setAttribute('class', 'userInfo');
@@ -55,12 +61,12 @@ class Users {
   }
 }
 
-function look() {
+function look() { // Look function used to see if a user has been selected
   const active = document.querySelectorAll('.active');
   console.log(active);
   return active;
 }
-
+/* Display on he menu the photo and name of the selected person */
 function usersDisplayActiveUser(data, id) {
   const name = 'name';
   const idUser = 'id_user';
@@ -83,11 +89,43 @@ function usersDisplayActiveUser(data, id) {
   }
 }
 
+function displayMessages(idReceiver) { // Display the message with the user
+  const myMessages = JSON.parse(JSON.stringify(myJsonMessages));
+  const me = document.querySelector('#personnal');
+  const messages = document.querySelector('#chat');
+  const idSender = me.getAttribute('data');
+  const date = new Date();
+  const dateHour = date.toLocaleString();
+  const stringSend = 'id_send';
+  const stringReceiver = 'id_received';
+  const stringMessage = 'message';
+
+  console.log(dateHour);
+  console.log(idReceiver);
+  console.log(idSender);
+  for (let i = 0; i < myMessages.past.length; i += 1) {
+    const eachMessage = myMessages.past[i][`${stringMessage}`];
+    const idMessageSender = myMessages.past[i][`${stringSend}`];
+    const idMessageReceiver = myMessages.past[i][`${stringReceiver}`];
+    const divMessage = document.createElement('div');
+    const textMessage = document.createTextNode(eachMessage);
+    if (idMessageSender === idSender && idMessageReceiver === idReceiver) {
+      divMessage.appendChild(textMessage);
+      divMessage.setAttribute('class', 'sender');
+      messages.appendChild(divMessage);
+    } else if (idMessageSender === idReceiver && idMessageReceiver === idSender) {
+      divMessage.appendChild(textMessage);
+      divMessage.setAttribute('class', 'receiver');
+      messages.appendChild(divMessage);
+    }
+  }
+  return myMessages;
+}
+
 function displayActiveTalk(id) {
-  console.log('test');
-  console.log(id);
   const mydata = JSON.parse(JSON.stringify(myJsonData));
   usersDisplayActiveUser(mydata, id);
+  displayMessages(id);
 }
 
 function displayRight() {
@@ -101,19 +139,15 @@ function displayRight() {
   }
 }
 
-function activeClass() {
-  const eachUser = document.querySelectorAll('.eachUser');
-  for (let i = 0; i < eachUser.length; i += 1) {
-    eachUser[i].addEventListener('click', () => {
-      displayRight();
-      if (look().length < 1) {
-        eachUser[i].classList.add('active');
-      } else {
-        look()[0].classList.remove('.active');
-        eachUser[i].classList.add('active');
-      }
-    });
-  }
+function sendMessage() {
+  console.log('test');
+}
+
+function cancelSend() {
+  document.querySelector('#send').addEventListener('click', (event) => {
+    event.preventDefault();
+    sendMessage();
+  });
 }
 
 function init() {
@@ -123,8 +157,19 @@ function init() {
     users.usersDisplay(mydata, i);
   }
   users.personnal(mydata);
-  activeClass();
-  displayRight();
+  cancelSend();
 }
 
 init();
+const eachUser = document.querySelectorAll('.eachUser');
+for (let i = 0; i < eachUser.length; i += 1) {
+  eachUser[i].addEventListener('click', () => {
+    displayRight();
+    if (look().length < 1) {
+      eachUser[i].classList.add('active');
+    } else {
+      look()[0].classList.remove('.active');
+      eachUser[i].classList.add('active');
+    }
+  });
+}

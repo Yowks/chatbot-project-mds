ce8d619393f4825d9d8436bd423185d4b43634cd
Yowks <48799889+Yowks@users.noreply.github.com>
2019-12-15 22:30:44 +0100

    :Add Photo active talking user, name, active class on list chat

--- src/index.js
+++ src/index.js
@@ -14,9 +14,12 @@ class Users {
     const name = 'name';
     const img = 'name_photo_user';
     const src = 'src/img/';
+    const id = 'id_user';
     const nameCreate = document.createTextNode(this.data.users[this.i][name]);
+    const idValue = this.data.users[this.i][id];
     const imgValue = this.data.users[this.i][img];
     divUser.setAttribute('class', 'eachUser');
+    divUser.setAttribute('data', `${idValue}`);
     divUserPhoto.setAttribute('class', 'eachUserPhoto');
     imgUserPhoto.setAttribute('src', `${src}${imgValue}`);
     divUserInfo.setAttribute('class', 'eachUserInfo');
@@ -52,6 +55,67 @@ class Users {
   }
 }
 
+function look() {
+  const active = document.querySelectorAll('.active');
+  console.log(active);
+  return active;
+}
+
+function usersDisplayActiveUser(data, id) {
+  const name = 'name';
+  const idUser = 'id_user';
+  const img = 'name_photo_user';
+  const src = 'src/img/';
+  for (let j = 0; j < data.users.length; j += 1) {
+    if (data.users[j][idUser] === id) {
+      const menuPhoto = document.querySelector('#menuUserPhoto');
+      const menuName = document.querySelector('#menuUserName');
+      const imgUserPhoto = document.querySelector('#imgUserPhoto');
+      const UserInfoName = document.createElement('h2');
+      const nameCreate = document.createTextNode(data.users[j][name]);
+      const imgValue = data.users[j][img];
+      menuName.innerHTML = '';
+      imgUserPhoto.setAttribute('src', `${src}${imgValue}`);
+      menuPhoto.appendChild(imgUserPhoto);
+      menuName.appendChild(UserInfoName);
+      UserInfoName.appendChild(nameCreate);
+    }
+  }
+}
+
+function displayActiveTalk(id) {
+  console.log('test');
+  console.log(id);
+  const mydata = JSON.parse(JSON.stringify(myJsonData));
+  usersDisplayActiveUser(mydata, id);
+}
+
+function displayRight() {
+  const right = document.getElementById('right');
+  if (look().length > 0) {
+    const id = look()[0].getAttribute('data');
+    right.style.display = 'block';
+    displayActiveTalk(id);
+  } else {
+    right.style.display = 'none';
+  }
+}
+
+function activeClass() {
+  const eachUser = document.querySelectorAll('.eachUser');
+  for (let i = 0; i < eachUser.length; i += 1) {
+    eachUser[i].addEventListener('click', () => {
+      displayRight();
+      if (look().length < 1) {
+        eachUser[i].classList.add('active');
+      } else {
+        look()[0].classList.remove('.active');
+        eachUser[i].classList.add('active');
+      }
+    });
+  }
+}
+
 function init() {
   const mydata = JSON.parse(JSON.stringify(myJsonData));
   const users = new Users();
@@ -59,6 +123,8 @@ function init() {
     users.usersDisplay(mydata, i);
   }
   users.personnal(mydata);
+  activeClass();
+  displayRight();
 }
 
 init();
